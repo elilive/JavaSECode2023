846856624e18d03a93a17acbf16230d28bfd522c
Eli <1163730440@qq.com>
2023-10-16 19:54:41 +0800

    first commit

---
+++ src/GitTest.java
@@ -0,0 +1,14 @@
+/**
+ * ClassName: GitTest
+ * Package: PACKAGE_NAME
+ * Description:
+ *
+ * @Author Eli-Chen
+ * @Create 2023/10/16 19:51
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
